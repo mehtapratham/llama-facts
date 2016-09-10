41cd14741abf8506d5ad11e12e70ec9fc8ac8be8
Pratham Mehta <pbm1992@gmail.com>
2016-09-10 17:53:52 -0400

    Update the look.

--- js/llama-facts.js
+++ js/llama-facts.js
@@ -1,16 +1,55 @@
 $(function() {
-	console.log( "ready!" );
 	$('.scroll-down-arrow').on('click.scroll-down-btn', onDownArrowClick);
 	$('.scroll-up-arrow').on('click.scroll-up-btn', onUpArrowClick);
+	setTimeout(function(){
+		updateNavigation();
+	},100);
 });
 
-var currentVisiblePage;
+var currentVisiblePage=0,
+arrPages=['.info-item.info-item-about-me','.info-item.info-item-about-me','.info-item.info-item-where-to-find-me'];
+
+
 
 function onDownArrowClick(event){
-	console.log('clicked');
-	$('.info-item.info-item-about-me').css('transform','translateY(0)');
+	currentVisiblePage++;
+	if(currentVisiblePage >= arrPages.length){
+		$('.scroll-down-arrow').hide();
+		currentVisiblePage = arrPages.length-1;
+	} else {
+		if(currentVisiblePage > 0){
+			$('.scroll-up-arrow').show();
+		}
+		$(arrPages[currentVisiblePage]).addClass('translate-item');
+		if(currentVisiblePage >= arrPages.length-1){
+			$('.scroll-down-arrow').hide();
+			currentVisiblePage = arrPages.length-1;
+		}
+	}
+	updateNavigation();
 }
 
 function onUpArrowClick(event){
-	console.log('up clicked');
+	if(currentVisiblePage <= 0){
+		$('.scroll-up-arrow').hide();
+		currentVisiblePage = 0;
+	} else {
+		if(currentVisiblePage < arrPages.length){
+			$('.scroll-down-arrow').show();
+		}
+		$(arrPages[currentVisiblePage]).removeClass('translate-item');
+		
+	}
+	currentVisiblePage--;
+	if(currentVisiblePage <= 0){
+		$('.scroll-up-arrow').hide();
+		currentVisiblePage = 0;
+	}
+	updateNavigation();
+}
+
+function updateNavigation(){
+	$('.nav-item-active').removeClass('nav-item-active');
+
+	$('.nav-item:nth-child('+(currentVisiblePage+1)+')').addClass('nav-item-active');
 }
